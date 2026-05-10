f12ffb47d6fa66636dab84cc56af45f70a940830
TE24B-Ligandereklof-Anton <anton.ligandereklof@elev.ga.ntig.se>
2026-05-10 16:13:43 +0200

    har lagt till bilder och text. Har dessutom gjort så att dark/light mode bevaras nät man byter sida/refreshar

--- script.js
+++ script.js
@@ -10,21 +10,35 @@ const darkColors = {
     '--detail': '#e9e9e9',
     '--text': '#fff',
 }
-applyColors(darkColors)
-var dark = true
+
 function applyColors(dict) {
     Object.keys(dict).forEach(function (key) {
         document.documentElement.style.setProperty(key, dict[key])
     })
 }
-function toggleDarkMode() {
-    dark = !dark
-    if (dark) {
+function applyCurrentTheme(){
+    if (localStorage.getItem("theme") == "light") {
         applyColors(darkColors)
         document.body.style.backgroundImage = 'url(./bilder/elden-ring-cover.avif)'
     }
-    else {
+    else if ((localStorage.getItem("theme") == "dark")){
         applyColors(lightColors)
         document.body.style.backgroundImage = 'url(./bilder/Gate_of_Divinity_Shadow_of_the_Erdtree.webp)'
     }
+    else{
+        localStorage.setItem("theme", "dark")
+        applyCurrentTheme()
+    }
 }
+function toggleDarkMode() {
+    if (localStorage.getItem("theme") == "light") {
+        localStorage.setItem("theme", "dark")
+        applyCurrentTheme()
+    }
+    else{
+        localStorage.setItem("theme", "light")
+        applyCurrentTheme()
+    }
+}
+
+applyCurrentTheme()
